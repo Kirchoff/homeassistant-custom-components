32548c8c8f9036c9f5bee5cba6eeac890d0670a8
Lincoln Kirchoff <lkirchoff@watlow.com>
2019-03-20 09:49:49 -0500

    Initial commit of source files and readme. hdd-script will need to be modified to accept cli arguments for pathing to smartctl.json, which is currently hardcoded.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # homeassistant-custom-components
-My custom components for home assistant
+My custom components for home assistant.
+
+# SmartCTL
+This custom component is made up of two parts: hdd-script.py and smartctl.py
+
+# hdd-script.py
+This script is designed to use the linux package smartmontools to report SMART data from your hard drives. It will discover all available /dev/sd* drives, and poll them with the smartctl binary. If the hard drive reports SMART data, it will be written in JSON format to smartctl.json. The path to smartctl.json should be provided via command line arguement to hdd-script.py. It will need to be ran at the interval that you want updated data to be provided to home assistant. (1 hour cronjob should be satisfactory in most cases)
+
+# smartctl.py
+This script is a home assistant sensor module. It will report temperature as the main sensor state, along with SMART attributes as sensor attributes. (5, 187, 194, 197, 198). It reads data provided from hdd-script.py in the form of the smartctl.json file.
---
+++ custom_components/sensor/smartctl.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+
+# home assistant imports
+from homeassistant.const import TEMP_CELSIUS
+from homeassistant.helpers.entity import Entity
+
+# generic imports
+import json
+
+# smartctl module
+DOMAIN = 'smartctl'
+
+# data source
+
+def setup_platform(hass, config, add_devices, discovery_info=None):
+    """Setup the sensor platform."""
+    json_file = open('/config/custom_components/sensor/smartctl.json', 'r')
+    for line in json_file:
+        json_line = json.loads(line)
+        add_devices([SmartCtl(json_line)])
+    json_file.close()
+
+
+
+
+class SmartCtl(Entity):
+    """Representation of a SmartCTL Sensor."""
+
+    def __init__(self, json_line):
+        """Initialize the sensor."""
+        # no leading underscores - otherwise use them
+        self._name = json_line["HDD"].replace('/','',1).replace('/','_')
+        self._path = json_line["HDD"]
+        self._temperature_celcius = json_line["Temperature_Celsius"]
+        self._attributes = {"reallocated_sectors": json_line["Reallocated_Sector_Ct"],
+                            "uncorrectable_errors": json_line["Uncorrectable_Error_Cnt"],
+                            "current_pending_sectors": json_line["Current_Pending_Sector"],
+                            "offline_uncorrectable_errors": json_line["Offline_Uncorrectable"]}
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return self._path
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._temperature_celcius
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit of measurement."""
+        return TEMP_CELSIUS
+
+    @property
+    def device_state_attributes(self):
+        """Return the state attributes."""
+        return self._attributes
+
+    def update(self):
+        """Fetch new state data for the sensor."""
+        json_file = open('/config/custom_components/sensor/smartctl.json', 'r')
+        for line in json_file:
+            json_line = json.loads(line)
+            if (json_line["HDD"] == self._path):
+                self._temperature_celcius = json_line["Temperature_Celsius"]
+                self._attributes = {"reallocated_sectors": json_line["Reallocated_Sector_Ct"],
+                            "uncorrectable_errors": json_line["Uncorrectable_Error_Cnt"],
+                            "current_pending_sectors": json_line["Current_Pending_Sector"],
+                            "offline_uncorrectable_errors": json_line["Offline_Uncorrectable"]}
+            else:
+                continue
+        json_file.close()
+
